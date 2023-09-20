824910e19089b9d38c368bdf7289e66cd649ef5e
Mac Mini <>
2023-09-20 07:07:40 +0600

    Project Create

---
+++ PicassoPro/PicassoProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicassoProApp.swift
+//  PicassoPro
+//
+//  Created by mac 2019 on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PicassoProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
